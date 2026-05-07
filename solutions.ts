6dc78242fcb8ec79fab2bc485b78550b6e89c8fd
Mamun123123123 <almalm90459@gmail.com>
2026-05-07 08:01:14 +0600

    reverse string

--- solutions.ts
+++ solutions.ts
@@ -2,3 +2,8 @@
 function filterEvenNumbers(numbers: number[]): number[] {
   return numbers.filter((num) => num % 2 === 0);
 }
+
+// Problem 2
+function reverseString(text: string): string {
+  return text.split("").reverse().join("");
+}
