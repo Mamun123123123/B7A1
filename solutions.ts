28667e93ba595c9fb039dda62c532c125eed57fa
Mamun123123123 <almalm90459@gmail.com>
2026-05-07 08:09:49 +0600

    getProperty and togglereadStatus

--- solutions.ts
+++ solutions.ts
@@ -1,20 +1,44 @@
 // Problem 1
-function filterEvenNumbers(numbers: number[]): number[] {
+
+function filterEvenNumbers( numbers: number[]): number[] {
   return numbers.filter((num) => num % 2 === 0);
 }
 
 // Problem 2
-function reverseString(text: string): string {
+
+function reverseString( text: string): string {
   return text.split("").reverse().join("");
 }
 
 // Problem 3
+
 type StringOrNumber = string | number;
 
-function checkType(value: StringOrNumber): string {
+function checkType( value: StringOrNumber): string {
   if (typeof value === "string") {
     return "String";
   }
 
   return "Number";
+}
+
+// Problem 4
+
+function getProperty( obj: any, key: string) {
+  return obj[key];
+}
+
+// Problem 5
+
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+}
+
+function  toggleReadStatus( book: Book): Book & { isRead: boolean } {
+  return {
+    ...book,
+    isRead: true,
+  };
 }
